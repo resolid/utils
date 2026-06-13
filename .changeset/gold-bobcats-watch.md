205661f5ff1f1534ef5c43951bdb3cfcdac99bb1
Huijie Wei <huijiewei@outlook.com>
2026-06-13 10:51:47 +0800

    feat: add MaybePromise and AtLeastOne utility types

---
+++ .changeset/gold-bobcats-watch.md
@@ -0,0 +1,5 @@
+---
+"@resolid/utils": patch
+---
+
+feat: add MaybePromise and AtLeastOne utility types
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,9 +2,15 @@ export type Booleanish = boolean | "true" | "false";
 
 export type MaybeArray<T> = T | T[];
 
+export type MaybePromise<T> = T | Promise<T>;
+
 export type Simplify<T> = { [K in keyof T]: T[K] } & {};
 
 export type ValueOrFunction<T, A extends unknown[] = []> = T | ((...args: A) => T);
+
+export type AtLeastOne<T> = {
+  [K in keyof T]-?: Required<Pick<T, K>> & Partial<Omit<T, K>>;
+}[keyof T];
 
 export class TimeoutError extends Error {
   override name = "TimeoutError";
